a68066da1372bbe51a87514c700f01a38f70f18c
tux <profgrumpy@gmail.com>
2020-05-15 13:40:11 -0400

    Resolved circular imports non-sense thanks to http://effbot.org/zone/import-confusion.htm Makes much more sense now ;p

--- datastore.py
+++ datastore.py
@@ -14,27 +14,8 @@ APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quizlib.sqlite'
 APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 DB = SQLAlchemy(APP)
 
-#TODO try to create the session only once & above
-
-
-class Quiz(DB.Model):
-    __tablename__ = 'quizzes'
-    id = DB.Column(DB.Integer, primary_key=True)
-    title = DB.Column(DB.String, nullable=False)
-    question = DB.Column(DB.String, nullable=False)
-    answer = DB.Column(DB.String, nullable=False)
-    def __repr__(self):
-        return "Quiz(id='%d',title='%s',question='%s',solution='%s')" % (self.id, self.title, self.question, self.answer)
-
-
-class Distractor(DB.Model):
-    __tablename__ = 'distractors'
-    id = DB.Column(DB.Integer, primary_key=True)
-    quiz_id = DB.Column(None, DB.ForeignKey('quizzes.id'))
-    answer = DB.Column(DB.String, nullable=False)
-    def __repr__(self):
-        return "Quiz(quiz_id=%d,id='%d',answer='%s')" % (self.quiz_id, self.id, self.answer)
-
+from models import Quiz, Distractor
+# doing this after db is defined to avoid circular imports
 
 class DataStore:
     
---
+++ models.py
@@ -0,0 +1,23 @@
+from datastore import DB
+#watch out for circular imports
+
+
+class Quiz(DB.Model):
+    __tablename__ = 'quizzes'
+    id = DB.Column(DB.Integer, primary_key=True)
+    title = DB.Column(DB.String, nullable=False)
+    question = DB.Column(DB.String, nullable=False)
+    answer = DB.Column(DB.String, nullable=False)
+    def __repr__(self):
+        return "Quiz(id='%d',title='%s',question='%s',solution='%s')" % (self.id, self.title, self.question, self.answer)
+
+
+class Distractor(DB.Model):
+    __tablename__ = 'distractors'
+    id = DB.Column(DB.Integer, primary_key=True)
+    quiz_id = DB.Column(None, DB.ForeignKey('quizzes.id'))
+    answer = DB.Column(DB.String, nullable=False)
+    def __repr__(self):
+        return "Quiz(quiz_id=%d,id='%d',answer='%s')" % (self.quiz_id, self.id, self.answer)
+
+
