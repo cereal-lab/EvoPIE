c74d6aeb24ea6d724c3239af495350202bc72776
tux <profgrumpy@gmail.com>
2020-05-23 14:04:16 -0400

    Adding form to index.html to test the addition of questions via POST/form instead of just POST/json

--- scripts/db_populate_via_REST.sh
+++ scripts/db_populate_via_REST.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# we just use curl to add two bogus distractors to quiz #2
-# this helps testing the acceptance of JSON formatted POST to the REST API
+# The questions' IDs are hardcoded, meaning that this script should be run 
+# right after starting the server on an empty database.
+# If there are already questions in the DB when this script is run, then
+# we will add distractors to the wrong questions
 
 
 # question #1
-curl -d '{ "title": "CURLed - Sir Lancelot and the bridge keeper, part 1", "question": "What... is your name?", "answer": "Sir Lancelot of Camelot"}' -H 'Content-Type: application/json' http://localhost:5000/questions
+curl -d '{ "title": "Sir Lancelot and the bridge keeper, part 1", "question": "What... is your name?", "answer": "Sir Lancelot of Camelot"}' -H 'Content-Type: application/json' http://localhost:5000/questions
 curl -d '{ "answer": "Sir Galahad of Camelot"}' -H 'Content-Type: application/json' http://localhost:5000/questions/1/distractors
 curl -d '{ "answer": "Sir Arthur of Camelot"}' -H 'Content-Type: application/json' http://localhost:5000/questions/1/distractors
 curl -d '{ "answer": "Sir Bevedere of Camelot"}' -H 'Content-Type: application/json' http://localhost:5000/questions/1/distractors
