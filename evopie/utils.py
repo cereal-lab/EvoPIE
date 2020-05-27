00299294e9f8ed5c4ec3ed5c9b7833c0c96bef07
tux <profgrumpy@gmail.com>
2020-05-27 18:05:42 -0400

    Refactoring - removed the remnants of datstore.py but kept its method to populate the DB as a utils.py function

---
+++ evopie/utils.py
@@ -0,0 +1,59 @@
+# pylint: disable=no-member
+# pylint: disable=E1101 
+# Dynamic classes like scoped_session are caught by pylint as not having any
+# of the members they end up featuring at runtime. This is a way to tell pylint to let it be
+
+from evopie import models # get also DB from there
+
+def DB_populate(self):
+    '''
+        Just populating the DB with some mock quizzes
+    '''
+    # For some reason Flask restarts the app when we launch it with
+    # pipenv run python inn.py
+    # as a result, we populate twice and get too many quizzes / distractors
+    # let's fix this by deleting all data from the tables first
+    models.Question.query.delete()
+    models.Distractor.query.delete()
+    models.DB.session.commit() # don't forget to commit or the DB will be locked
+
+    all_mcqs = [
+            models.Question(title=u'Sir Lancelot and the bridge keeper, part 1',
+                            question=u'What... is your name?',
+                            answer=u'Sir Lancelot of Camelot'),
+            models.Question(title=u'Sir Lancelot and the bridge keeper, part 2',
+                            question=u'What... is your quest?', 
+                            answer=u'To seek the holy grail'),
+            models.Question(title=u'Sir Lancelot and the bridge keeper, part 3',
+                            question=u'What... is your favorite colour?', 
+                            answer=u'Blue')
+    ]
+    
+    models.DB.session.add_all(all_mcqs)
+    models.DB.session.commit()        
+    # need to commit right now; if not, the qid below will not be added in the distractors table's rows
+
+    qid=all_mcqs[0].id
+    some_distractors = [
+        models.Distractor(question_id=qid,answer=u'Sir Galahad of Camelot'),
+        models.Distractor(question_id=qid,answer=u'Sir Arthur of Camelot'),
+        models.Distractor(question_id=qid,answer=u'Sir Bevedere of Camelot'),
+        models.Distractor(question_id=qid,answer=u'Sir Robin of Camelot'),
+    ]
+    
+    qid=all_mcqs[1].id
+    more_distractors = [
+        models.Distractor(question_id=qid,answer=u'To bravely run away'),
+        models.Distractor(question_id=qid,answer=u'To spank Zoot'),
+        models.Distractor(question_id=qid,answer=u'To find a shrubbery')
+    ]
+
+    qid=all_mcqs[2].id
+    yet_more_distractors = [
+        models.Distractor(question_id=qid,answer=u'Green'),
+        models.Distractor(question_id=qid,answer=u'Red'),
+        models.Distractor(question_id=qid,answer=u'Yellow')
+    ]
+
+    models.DB.session.add_all(some_distractors + more_distractors + yet_more_distractors)
+    models.DB.session.commit()
