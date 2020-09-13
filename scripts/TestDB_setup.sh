a8c3689b2c4badaf0bf868aec17de1ba3d62089f
tux <profgrumpy@gmail.com>
2020-09-13 16:19:55 -0400

    scripts performing setup / step 1 / step 2 completed.

---
+++ scripts/TestDB_setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# PRE-REQUISITES
+# run this script right after starting the server with
+# flask DB-reboot && flask-run
+
+source ./TestDB_functions.sh
+
+
+header 'SIGNING UP INSTRUCTOR & STUDENT ACCOUNTS, THEN LOGIN AS INSTRUCTOR'
+#FIXME for now, we hardcode that user id 1 is an instructor; so we must sign him up first.
+curlit "signed up instructor"       "/signup"           '{ "email": "instructor@usf.edu", "password": "instructor", "firstname": "John", "lastname": "Keating" }'
+curlit "signed up student #1"       "/signup"           '{ "email": "student1@usf.edu", "password": "student1", "firstname": "Anakin", "lastname": "Skywalker" }'
+curlit "signed up student #2"       "/signup"           '{ "email": "student2@usf.edu", "password": "student2", "firstname": "Ahsoka", "lastname": "Tano" }'
+curlit "signed up student #3"       "/signup"           '{ "email": "student3@usf.edu", "password": "student3", "firstname": "Obi-Wan", "lastname": "Kenobi" }'
+curl_login                          "instructor"        '{ "email": "instructor@usf.edu", "password": "instructor" }'
+
+header 'ADDING QUESTION #1' #QID 1
+curlit  'Added Question'            "/questions"                    '{ "title": "Sir Lancelot and the bridge keeper, part 1", "stem": "What... is your name?", "answer": "Sir Lancelot of Camelot"}'
+curlit  'Added distractor'          "/questions/1/distractors"      '{ "answer": "Sir Galahad of Camelot"}'         #D1
+curlit  'Added distractor'          "/questions/1/distractors"      '{ "answer": "Sir Arthur of Camelot"}'          #D2
+curlit  'Added distractor'          "/questions/1/distractors"      '{ "answer": "Sir Bevedere of Camelot"}'        #D3
+curlit  'Added distractor'          "/questions/1/distractors"      '{ "answer": "Sir Robin of Camelot"}'           #D4
+curlit  'QuizQuestion composed'     "/quizquestions"                '{ "qid": "1", "distractors_ids": [1, 2, 3, 4]}'
+
+header 'ADDING QUESTION #2' #QID 2
+curlit  'Added Question'            "/questions"                    '{ "title": "Sir Lancelot and the bridge keeper, part 2", "stem": "What... is your quest?", "answer": "To seek the holy grail"}'
+curlit  'Added distractor'          "/questions/2/distractors"      '{ "answer": "To bravely run away"}'            #D5
+curlit  'Added distractor'          "/questions/2/distractors"      '{ "answer": "To spank Zoot"}'                  #D6
+curlit  'Added distractor'          "/questions/2/distractors"      '{ "answer": "To find a shrubbery"}'            #D7
+curlit  'QuizQuestion composed'     "/quizquestions"                '{ "qid": "2", "distractors_ids": [5, 6, 7]}'
+
+header 'ADDING QUESTION #3' #QID 3
+curlit  'Added Question'            "/questions"                    '{ "title": "Sir Lancelot and the bridge keeper, part 3", "stem": "What... is your favorite colour?", "answer": "Blue"}'
+curlit  'Added distractor'          "/questions/3/distractors"      '{ "answer": "Green"}'                          #D8
+curlit  'Added distractor'          "/questions/3/distractors"      '{ "answer": "Red"}'                            #D9
+curlit  'Added distractor'          "/questions/3/distractors"      '{ "answer": "Yellow"}'                         #D10
+curlit  'QuizQuestion composed'     "/quizquestions"                '{ "qid": "3", "distractors_ids": [8, 9, 10]}'
+
+header 'CREATING QUIZ #1 from above questions'
+curlit  'Quiz Created'              "/quizzes"          '{ "title": "Test Quiz", "description": "This is just to make sure things are working", "questions_ids":[3, 2, 1]}'
+
+# new question template
+# curlit  'Added Question'            "/questions"        '{ "title": "", "stem": "", "answer": ""}'
+# curlit  'Added distractor'          "/distractors"      '{ "answer": ""}'
+# curlit  'Added distractor'          "/distractors"      '{ "answer": ""}'
+# curlit  'Added distractor'          "/distractors"      '{ "answer": ""}'
+# curlit  'QuizQuestion composed'     "/quizquestions"    '{ "qid": "2", "distractors_ids": [6, 5, 4]}'
---
+++ scripts/TestDB_step2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# PRE-REQUISITES
+#   ./scripts/TestDB_setup.sh
+#   ./scripts/TestDB_step1.sh
+
+source ./TestDB_functions.sh
+
+# step #2
+header "Step #2 for Student #1"
+curl_login              "student #1"        '{ "email": "student1@usf.edu", "password": "student1"}'
+curlit "GET quiz"       "/quizzes/1/take"   ''
+curlit "POST quiz"      "/quizzes/1/take"   '{ "revised_responses": {"1":"-1" , "2":"6", "3":"9"} }'
+
+header "Step #2 for Student #2"
+curl_login              "student #2"        '{ "email": "student2@usf.edu", "password": "student2"}'
+curlit "GET quiz"       "/quizzes/1/take"   ''
+curlit "POST quiz"      "/quizzes/1/take"   '{ "revised_responses": {"1":"3" , "2":"-1", "3":"10"} }'
+
+header "Step #2 for Student #3"
+curl_login              "student #3"        '{ "email": "student3@usf.edu", "password": "student3"}'
+curlit "GET quiz"       "/quizzes/1/take"   ''
+curlit "POST quiz"      "/quizzes/1/take"   '{ "revised_responses": {"1":"4" , "2":"7", "3":"-1"} }'
