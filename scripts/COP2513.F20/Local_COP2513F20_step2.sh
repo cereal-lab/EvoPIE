ffd8f0fd77d37a742d1b9e91e6f5291b39d9ed3d
Alessio Gaspar <profgrumpy@gmail.com>
2020-11-27 01:52:08 -0500

    Feng-shuing the scripts so that all the COP2513.F20 stuff is altogether.

--- scripts/COP2513.F20/Local_COP2513F20_step2.sh
+++ scripts/COP2513.F20/Local_COP2513F20_step2.sh
@@ -3,7 +3,7 @@
 # PRE-REQUISITES
 #   ./scripts/Test_COP2513F20_setup.sh
 
-source ./TestDB_functions.sh
+source ../TestDB_functions.sh
 
 #login as INSTRUCTOR to release quiz in step #2
 curl_login                          "instructor"            '{ "email": "instructor@usf.edu", "password": "instructor" }'
