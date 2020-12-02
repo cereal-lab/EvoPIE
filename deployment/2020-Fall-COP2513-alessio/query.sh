7a8aae426903acee2ff014bf1acbcbbbb3a83fdf
Alessio Gaspar <profgrumpy@gmail.com>
2020-12-01 20:19:37 -0500

    Archiving results of first test-run.

---
+++ deployment/2020-Fall-COP2513-alessio/query.sh
@@ -0,0 +1,4 @@
+#!/bin.bash
+
+# Provide name of sqlite file as argument
+sqlite3 $1 "select user.last_name, user.first_name, user.email,quiz_attempt.initial_responses, quiz_attempt.revised_responses from user,quiz_attempt on user.id==quiz_attempt.student_id order by user.last_name COLLATE NOCASE;"
