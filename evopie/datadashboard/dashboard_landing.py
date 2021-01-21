7841723c1dc3cf966d882d6cbb1610ab8f9fff90
rpwiegand <rpwiegand@gmail.com>
2021-01-21 15:19:35 +0000

    Small changes to data dahsboard page

--- evopie/datadashboard/dashboard_landing.py
+++ evopie/datadashboard/dashboard_landing.py
@@ -4,6 +4,7 @@
 from .. import models, APP # get also DB from there
 
 import click
+import os
 
 '''
 This is a hypothetical landing page for the data dashboard.
@@ -17,4 +18,25 @@ def dataDashboard():
     A test route to make sure we understand how to hook into the
     overall web server.  This simply displays a header message
     '''
-    return '<html><body><h1>EvoPI Data Analysis Dashboard</h1></body></html>'
+
+    HTMLTextLines = []
+
+    HTMLTextLines.append('<html>')
+    HTMLTextLines.append('<head>')
+    HTMLTextLines.append('  <title>EvoPIE Data Analysis Dashboard</title>')
+    HTMLTextLines.append('</head>')
+    HTMLTextLines.append('')
+    HTMLTextLines.append('<body>')
+    HTMLTextLines.append('  <h1>EvoPIE Data Analysis Dashboard</h1>')
+    HTMLTextLines.append('  <p>Silly text</p>')
+
+    # Here I'm just using the HTML page to see how to access the quizzes DB
+    HTMLTextLines.append('  <samp>')
+    myQuiz = models.Quiz()
+    HTMLTextLines.append( str(myQuiz.dump_as_dict()) )
+    HTMLTextLines.append('  </samp>')
+
+    HTMLTextLines.append('</body>')
+    HTMLTextLines.append('</html>')
+
+    return os.linesep.join(HTMLTextLines)
