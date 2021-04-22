97e0cb7bed12bd093cd8220d482deac27d2a4f54
Alessio Gaspar <profgrumpy@gmail.com>
2021-04-22 01:26:35 -0400

    Improving query.sh script so as to count the number of -1 per line (right answers).

--- deployment/2021-spring/query.sh
+++ deployment/2021-spring/query.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-# Provide name of sqlite file as argument
-sqlite3 $1 "select user.last_name, user.first_name, user.email,quiz_attempt.initial_responses, quiz_attempt.revised_responses from user,quiz_attempt on user.id==quiz_attempt.student_id order by user.last_name COLLATE NOCASE;"
+if [[ $# -ne 1 ]]
+then
+    echo "ERROR - must provide 1 argument: sqlite3 file name"
+    exit
+fi
+
+FILE=$1
+
+if [[ ! -f $FILE ]]
+then
+    echo "ERROR - File $FILE does not exist"
+    exit
+fi
+
+QUERY="select user.last_name, user.first_name, user.email,quiz_attempt.initial_responses, quiz_attempt.revised_responses from user,quiz_attempt on user.id==quiz_attempt.student_id order by user.last_name COLLATE NOCASE;"
+sqlite3 $1 "$QUERY" | awk '$0 ~ FS { print NF-1, $0 }' IGNORECASE=1 FS="-1" 
+# awk is used to display the number of correct answers per line
+
+
+
