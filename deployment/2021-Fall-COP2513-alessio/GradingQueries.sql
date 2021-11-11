98f1d3525bc4db40311870f882ccc89f8b476f23
Alessio Gaspar <profgrumpy@gmail.com>
2021-11-10 20:58:52 -0500

    Using MySQL plugin for visual studio code to grade.

---
+++ deployment/2021-Fall-COP2513-alessio/GradingQueries.sql
@@ -0,0 +1,32 @@
+# query to process performance on step 1 & 2 of a given quiz
+QUERY="SELECT user.last_name, user.first_name, user.email,quiz_attempt.initial_responses, quiz_attempt.revised_responses FROM user,quiz_attempt,quiz ON user.id==quiz_attempt.student_id AND quiz.id==quiz_attempt.quiz_id AND quiz.title==\"PLQ2\" ORDER BY user.last_name COLLATE NOCASE;"
+
+#all students with their performance computed on step 1
+sqlite3 DB_quizlib.sqlite "$QUERY" | awk '$0 { printf "%-20s\t%-10s\t%s\n",$1,$2,$4}' FS="|" | awk '$0 ~ FS { print NF-1, $0 }' IGNORECASE=1 FS="-1" | less
+
+# all students with their performance computed on step #2
+sqlite3 DB_quizlib.sqlite "$QUERY" | awk '$0 { printf "%-20s\t%-10s\t%s\n",$1,$2,$5}' FS="|" | awk '$0 ~ FS { print NF-1, $0 }' IGNORECASE=1 FS="-1"| less
+
+
+# listing all justification to be able to grade by hand
+sqlite3 DB_quizlib.sqlite "SELECT user.last_name, user.first_name, user.email, justification.quiz_question_id, justification.justification FROM user,quiz,quiz_attempt,justification ON (justification.quiz_question_id==\"10\" OR justification.quiz_question_id==\"11\" OR justification.quiz_question_id==\"12\") AND justification.student_id==user.id AND user.id==quiz_attempt.student_id AND quiz.id==quiz_attempt.quiz_id AND quiz.title==\"PLQ1\" AND user.id==166 ORDER BY user.last_name COLLATE NOCASE;" | less
+
+#using the "mySQL" visual studio code plugin views below to grade
+CREATE VIEW PLQ_2_performance
+AS
+SELECT user.last_name, user.first_name, user.email,quiz_attempt.initial_responses, quiz_attempt.revised_responses
+FROM user,quiz_attempt,quiz
+ON user.id==quiz_attempt.student_id AND quiz.id==quiz_attempt.quiz_id AND quiz.title=="PLQ2"
+ORDER BY user.last_name COLLATE NOCASE;
+
+
+CREATE VIEW PLQ_2_justifications
+AS
+SELECT user.last_name, user.first_name, user.email, justification.quiz_question_id, justification.justification
+FROM user,quiz,quiz_attempt,justification
+ON  (justification.quiz_question_id=="10" OR justification.quiz_question_id=="11" OR justification.quiz_question_id=="12")
+    AND justification.student_id==user.id
+    AND user.id==quiz_attempt.student_id
+    AND quiz.id==quiz_attempt.quiz_id
+    AND quiz.title=="PLQ2"
+ORDER BY user.last_name COLLATE NOCASE;
