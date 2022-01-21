4341b5ae77c94b9595213e5bd7c1bb57f512c501
Alessio Gaspar <profgrumpy@gmail.com>
2022-01-21 15:20:00 -0500

    Fixing old testing scripts.
    Problems included not having the retype field simulated in the form. It was added way after the scripts were written.

--- testing/2022-spring/mayank-testing.sh
+++ testing/2022-spring/mayank-testing.sh
@@ -7,18 +7,18 @@ source ./TestDB_functions.sh
 
 header 'SIGNING UP INSTRUCTOR & STUDENT ACCOUNTS, THEN LOGIN AS INSTRUCTOR'
 #FIXME for now, we hardcode that user id 1 is an instructor; so we must sign him up first.
-#curlit "signed up instructor"       "/signup"           '{ "email": "instructor@usf.edu", "password": "instructor", "firstname": "John", "lastname": "Keating" }'
-##curlit "signed up student #1"       "/signup"           '{ "email": "student1@usf.edu", "password": "student1", "firstname": "Anakin", "lastname": "Skywalker" }'
-##curlit "signed up student #2"       "/signup"           '{ "email": "student2@usf.edu", "password": "student2", "firstname": "Ahsoka", "lastname": "Tano" }'
-##curlit "signed up student #3"       "/signup"           '{ "email": "student3@usf.edu", "password": "student3", "firstname": "Obi-Wan", "lastname": "Kenobi" }'
-##curl_login                          "instructor"        '{ "email": "alessio@usf.edu", "password": "student1" }'
+curlit "signed up instructor"       "/signup"           '{ "email": "instructor@usf.edu", "password": "instructor", "retype": "instructor","firstname": "John", "lastname": "Keating" }'
+curlit "signed up student #1"       "/signup"           '{ "email": "student1@usf.edu", "password": "student1", "retype": "student1","firstname": "Anakin", "lastname": "Skywalker" }'
+curlit "signed up student #2"       "/signup"           '{ "email": "student2@usf.edu", "password": "student2", "retype": "student2", "firstname": "Ahsoka", "lastname": "Tano" }'
+curlit "signed up student #3"       "/signup"           '{ "email": "student3@usf.edu", "password": "student3", "retype": "student3","firstname": "Obi-Wan", "lastname": "Kenobi" }'
+curl_login                          "instructor"        '{ "email": "instructor@usf.edu", "password": "instructor" }'
 
 ##header 'ADDING QUESTION #1' #QID 1
 ##curlit  'Added Question'            "/questions"                    '{ "title": "Sir Lancelot and the bridge keeper, part 1", "stem": "What... is your name?", "answer": "Sir Lancelot of Camelot"}'
-##curlit  'Added distractor'          "/questions/1/distractors"      '{ "answer": "Sir Galahad of Camelot"}'         #D10
-##curlit  'Added distractor'          "/questions/1/distractors"      '{ "answer": "Sir Arthur of Camelot"}'          #D11
-##curlit  'Added distractor'          "/questions/1/distractors"      '{ "answer": "Sir Bevedere of Camelot"}'        #D12
-##curlit  'Added distractor'          "/questions/1/distractors"      '{ "answer": "Sir Robin of Camelot"}'           #D13
+##curlit  'Added distractor'          "/questions/4/distractors"      '{ "answer": "Sir Galahad of Camelot"}'         #D10
+##curlit  'Added distractor'          "/questions/4/distractors"      '{ "answer": "Sir Arthur of Camelot"}'          #D11
+##curlit  'Added distractor'          "/questions/4/distractors"      '{ "answer": "Sir Bevedere of Camelot"}'        #D12
+##curlit  'Added distractor'          "/questions/4/distractors"      '{ "answer": "Sir Robin of Camelot"}'           #D13
 ##curlit  'QuizQuestion composed'     "/quizquestions"                '{ "qid": "4", "distractors_ids": [10, 11, 12, 13]}'
 
 #header 'ADDING QUESTION #2' #QID 2
@@ -35,8 +35,8 @@ header 'SIGNING UP INSTRUCTOR & STUDENT ACCOUNTS, THEN LOGIN AS INSTRUCTOR'
 #curlit  'Added distractor'          "/questions/3/distractors"      '{ "answer": "Yellow"}'                         #D10
 #curlit  'QuizQuestion composed'     "/quizquestions"                '{ "qid": "3", "distractors_ids": [8, 9, 10]}'
 
-header 'CREATING QUIZ #1 from above questions'
-curlit  'Quiz Created'              "/quizzes"          '{ "title": "Test Quiz", "description": "This is just to make sure things are working", "questions_ids":[4]}'
+#header 'CREATING QUIZ #1 from above questions'
+#curlit  'Quiz Created'              "/quizzes"          '{ "title": "Test Quiz", "description": "This is just to make sure things are working", "questions_ids":[4]}'
 
 # new question template
 # curlit  'Added Question'            "/questions"        '{ "title": "", "stem": "", "answer": ""}'
