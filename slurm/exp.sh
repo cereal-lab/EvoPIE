a4c11b7dcf3314378a2fa2187373e5347acf2507
dvitel <viteldmitry@gmail.com>
2022-07-29 22:40:51 -0400

    small fixes

--- slurm/exp.sh
+++ slurm/exp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
-#SBATCH --job-name=deca-spaces
+#SBATCH --job-name=deca-vs-pphc
 #SBATCH --time=24:00:00
-#SBATCH --output=deca-spaces-%a.out
+#SBATCH --output=deca-vs-pphc-%a.out
 #SBATCH --mem=8G
 #SBATCH --array=2-10
 
