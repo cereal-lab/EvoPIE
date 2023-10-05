4b699563dcde21f620442a69880c4879dd933499
RLucas1102 <robenoltl@gmail.com>
2023-10-04 22:11:50 -0400

    Added glossary for dashboard

---
+++ evopie/datadashboard/datalayer/updateglossary.py
@@ -0,0 +1,139 @@
+from sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import select
+from sqlalchemy import insert
+from sqlalchemy import MetaData
+from sqlalchemy import inspect
+from sqlalchemy.orm import Session
+
+import json
+import sys
+
+DB = SQLAlchemy()
+
+ROLE_STUDENT = "STUDENT"
+ROLE_INSTRUCTOR = "INSTRUCTOR"
+ROLE_ADMIN = "ADMIN"
+
+
+class GlossaryTerm(DB.Model):
+   """
+   The ORM secheme for glossary terms. 
+   """
+   __tablename__ = "glossary"
+
+   id = DB.Column(DB.Integer, primary_key=True)
+   term = DB.Column(DB.String, nullable=False)
+   definition = DB.Column(DB.String, nullable=False)
+
+
+def get_database_session(dbFilename):
+  """
+  Connect to the database, return the session object
+  """
+  connectionString = "sqlite:///" + dbFilename
+  engine = None
+  session = None
+
+  try:
+    engine = create_engine(connectionString)
+    session = Session(engine)
+
+  except:
+    print("ERROR: Could not open DB at '" + connectionString + "'.")
+    sys.exit(11)
+
+  print("Connected to: ", connectionString)
+
+  try:
+    # If the glossary table does not exist, create it
+    if not inspect(engine).has_table("glossary"): 
+        print("  The 'glossary' table did not exist.  Creating it.") 
+        metadata = MetaData() #   MetaData(engine)
+        DB.Table( "glossary", metadata,
+            DB.Column('id', DB.Integer, primary_key=True), 
+            DB.Column('term', DB.String, nullable=False),
+            DB.Column('definition', DB.String, nullable=False) )
+        metadata.create_all(engine)   
+    else:
+      print("  There was already a 'glossary' table.  No need to create it.") 
+  except:
+    print("ERROR: Could not create the glossary table")
+    sys.exit(12)
+
+  return session
+
+
+def get_glossary_dict(jsonFilename):
+  """
+  Go get the JSON file and return a dictionary
+  """
+  jsonDict = None
+  try:
+    jsonFile = open(jsonFilename)
+    jsonDict = json.load(jsonFile)
+  except:
+    print("ERROR: Could not find a JSON file at '" + jsonFilename + "'.")
+    sys.exit(1)
+
+  try:
+    ids = jsonDict['ID']
+    terms = jsonDict['Term']
+    definitions = jsonDict['Definition']
+
+    tmpDict = {}
+    for idString in ids:
+      tmpDict[int(idString)] = (terms[idString], definitions[idString])
+
+    jsonDict = tmpDict
+
+  except:
+    print("ERROR:  The JSON file at '" + jsonFilename + "' isn't formatted as expected.")
+    sys.exit(2)
+
+  return jsonDict
+
+
+def update_glossary_table(dbSession, jsonDict):
+  """"""
+  try:
+    for jid in jsonDict:
+        jterm, jdefinition = jsonDict[jid]
+
+        ## Check if jid is in the glossary table  If not, add, if so update via ORM
+        try:
+          luTerm = dbSession.query(GlossaryTerm).filter_by(id=jid).one()
+
+          if (not jterm == luTerm.term) or (not jdefinition == luTerm.definition):
+            luTerm.term = jterm
+            luTerm.definition = jdefinition
+            print("  Updating term '" + luTerm.term + "'.")
+          else:
+            print("  Skipping term '" + jterm + "'.")
+
+        except:
+          print("  Adding term '" + jterm + "'")
+          glossaryTerm = GlossaryTerm(id=jid, term=jterm, definition=jdefinition)
+          dbSession.add(glossaryTerm)
+
+  except:
+    print("ERROR:  There was a database access problem.")
+    sys.exit(4)
+    
+  print("Updated the glossary table.")
+
+
+if __name__ == '__main__':
+  jsonFilename = "../../../glossary.json"
+  dbFilename = "../../DB_quizlib.sqlite"
+
+  try:
+    jsonFilename = sys.argv[1]
+    dbFilename = sys.argv[2]
+  except:
+    pass
+  
+  glossaryDict = get_glossary_dict(jsonFilename)
+  dbSession = get_database_session(dbFilename)
+  update_glossary_table(dbSession, glossaryDict)
+  dbSession.commit()
