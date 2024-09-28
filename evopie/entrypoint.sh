4fccdd4170a9cef4e94313c962cac331572ac153
Alessio Gaspar <profgrumpy@gmail.com>
2024-09-28 15:38:28 -0400

    Reverting previous change.
    The syntax seems to have been deprecated. Setting DEBUG to True in .env though.

--- evopie/entrypoint.sh
+++ evopie/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 pipenv run flask DB-init
 pipenv run python3 datalayer/updateglossary.py /app/data/db.sqlite
-#pipenv run gunicorn --log-level debug -w 4 -b 0.0.0.0:5000 app:APP
-pipenv run gunicorn --reload --workers 1 -b 0.0.0.0:5000 app:APP --debug --debugger-address="0.0.0.0:5678"
+pipenv run gunicorn --log-level debug -w 4 -b 0.0.0.0:5000 app:APP
+# seems to be old syntax:
+#pipenv run gunicorn --reload --workers 1 -b 0.0.0.0:5000 app:APP --debug --debugger-address="0.0.0.0:5678"
