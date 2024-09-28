563ddead73601459dd708e4f056570f4d3f9f7af
Alessio Gaspar <profgrumpy@gmail.com>
2024-09-28 16:29:35 -0400

    Integrating debugpy.
    Also setting up debugging in app.py based on the environmental variable DEBUG.

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from evopie import APP
 from evopie.cli import DB_init, DB_populate
 
+import debugpy
+import os
+
 # Winthrop testing page ...
 
 if __name__ == '__main__':
-    APP.run(debug=True)
+    debug_mode = os.environ.get("DEBUG")
+    if debug_mode == "True":
+        # to enable remote debugging into the app: 
+        debugpy.listen(("0.0.0.0", 5678))
+        APP.run(debug=True)
+    else:
+        APP.run()
     APP.cli.add_command(DB_init)
     APP.cli.add_command(DB_populate)
