e630d59c13d4bc176d99f2f08daa79a75b0b94fb
Alessio Gaspar <profgrumpy@gmail.com>
2024-09-28 16:41:34 -0400

    Adding debug entrypoint.

--- evopie/entrypoint.sh
+++ evopie/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash 
 pipenv run flask DB-init
 pipenv run python3 datalayer/updateglossary.py /app/data/db.sqlite
-pipenv run gunicorn --log-level debug -w 4 -b 0.0.0.0:5000 app:APP
+# Production version: 
+# pipenv run gunicorn -w 4 -b 0.0.0.0:5000 app:APP
+# Dev version: 
+pipenv run gunicorn --log-level debug -w 1 -b 0.0.0.0:5000 app:APP
 # seems to be old syntax:
 #pipenv run gunicorn --reload --workers 1 -b 0.0.0.0:5000 app:APP --debug --debugger-address="0.0.0.0:5678"
+
