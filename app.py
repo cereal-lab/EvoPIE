0aaf0b69c88ac5465afd24735e646256c1fcb9ce
Alessio Gaspar <profgrumpy@gmail.com>
2024-09-28 17:39:48 -0400

    Commenting out the DebugPY setup that was in the wrong spot. See previous commit for actual solution.

--- app.py
+++ app.py
@@ -7,14 +7,17 @@ import os
 # Winthrop testing page ...
 
 if __name__ == '__main__':
-    debug_mode = os.environ.get("DEBUG")
-    if debug_mode == "True":
-        # to enable remote debugging into the app: 
-        print('DEBUGPY ACTIVELY LISTENING')
-        debugpy.listen(("0.0.0.0", 5678))
-        APP.run(debug=True)
-    else:
-        print('DEBUGPY NOT DOING ANYTHING')
-        APP.run()
+    # not the right spot to do the following
+    # See instead __init__.py in evopie
+    # debug_mode = os.environ.get("DEBUG")
+    #if debug_mode == "True":
+    #    # to enable remote debugging into the app: 
+    #    print('DEBUGPY ACTIVELY LISTENING')
+    #    debugpy.listen(("0.0.0.0", 5678))
+    #    APP.run(debug=True)
+    #else:
+    #    print('DEBUGPY NOT DOING ANYTHING')
+    #    APP.run()
+    APP.run(debug=True)
     APP.cli.add_command(DB_init)
     APP.cli.add_command(DB_populate)
