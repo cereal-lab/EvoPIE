76773131805babb6fe91a9717e3706563ca9a604
Alessio Gaspar <profgrumpy@gmail.com>
2024-09-28 14:57:36 -0400

    Turning on debug mode in entrypoint.sh

--- evopie/entrypoint.sh
+++ evopie/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
 pipenv run flask DB-init
 pipenv run python3 datalayer/updateglossary.py /app/data/db.sqlite
-pipenv run gunicorn -w 4 -b 0.0.0.0:5000 app:APP
+#pipenv run gunicorn --log-level debug -w 4 -b 0.0.0.0:5000 app:APP
+pipenv run gunicorn --reload --workers 1 -b 0.0.0.0:5000 app:APP --debug --debugger-address="0.0.0.0:5678"
