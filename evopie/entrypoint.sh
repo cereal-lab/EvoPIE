48633b3353b22b69a43625adfcca209e48249543
rpwiegand <rpwiegand@gmail.com>
2025-08-12 15:32:29 -0400

    Putting port back so I don't break anyone else's work

--- evopie/entrypoint.sh
+++ evopie/entrypoint.sh
@@ -4,8 +4,8 @@ pipenv run python3 datalayer/updateglossary.py /app/data/db.sqlite
 # Production version: 
 # pipenv run gunicorn -w 4 -b 0.0.0.0:5000 app:APP
 # Dev version: 
-#pipenv run gunicorn --log-level debug -w 1 -b 0.0.0.0:5000 app:APP
-pipenv run gunicorn --log-level debug -w 1 -b 0.0.0.0:5010 app:APP
+pipenv run gunicorn --log-level debug -w 1 -b 0.0.0.0:5000 app:APP
+#pipenv run gunicorn --log-level debug -w 1 -b 0.0.0.0:5010 app:APP
 # seems to be old syntax:
 #pipenv run gunicorn --reload --workers 1 -b 0.0.0.0:5000 app:APP --debug --debugger-address="0.0.0.0:5678"
 
