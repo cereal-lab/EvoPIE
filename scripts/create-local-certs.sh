5761bba84aae5da32ce41da0afb25052f78c8230
kalyanoliveira <kalyan.coliveira@gmail.com>
2026-09-11 15:58:23 +0000

    feat: adds local certificate helper

---
+++ scripts/create-local-certs.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -eu
+
+CERT_DOMAIN="${EVOPIE_CERT_DOMAIN:-evopie.cse.usf.edu}"
+CERTS_DIR="${EVOPIE_CERTS_DIR:-./certs}"
+CERT_DIR="$CERTS_DIR/live/$CERT_DOMAIN"
+
+mkdir -p "$CERT_DIR"
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -keyout "$CERT_DIR/privkey.pem" \
+  -out "$CERT_DIR/fullchain.pem" \
+  -subj "/CN=$CERT_DOMAIN"
+
+cat <<EOF
+Created local self-signed certificate files:
+
+  $CERT_DIR/fullchain.pem
+  $CERT_DIR/privkey.pem
+
+Start HTTPS mode with:
+
+  EVOPIE_NGINX_MODE=https \\
+  EVOPIE_SERVER_NAME=$CERT_DOMAIN \\
+  EVOPIE_CERT_DOMAIN=$CERT_DOMAIN \\
+  EVOPIE_CERTS_DIR=$CERTS_DIR \\
+  docker compose up --build -d
+EOF
